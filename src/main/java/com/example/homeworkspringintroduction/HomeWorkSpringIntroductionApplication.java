0401864de4a6fac3fccbb2484f6136c9b617ae4d
andrey-satyukov <1846120@gmail.com>
2023-05-15 12:36:16 +0500

    первый коммит

---
+++ src/main/java/com/example/homeworkspringintroduction/HomeWorkSpringIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkspringintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkSpringIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkSpringIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homeworkspringintroduction/HomeWorkSpringIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkspringintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkSpringIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
